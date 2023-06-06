5568f56f0ddbb3da163b45ec0f378d37df966465
redlroses <redlroses.rlr@gmail.com>
2023-06-06 16:06:22 +0500

    Fix camera blur

--- Assets/Code/Logic/CameraMovement.cs
+++ Assets/Code/Logic/CameraMovement.cs
@@ -1,33 +1,28 @@
 using NTC.Global.Cache;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Logic
 {
     public class CameraMovement : MonoCache
     {
-        [FormerlySerializedAs("distance")] [SerializeField] private float _distance = 20;
-        [FormerlySerializedAs("cameraOffset")] [SerializeField] private Vector3 _cameraOffset;
-        [SerializeField] private float _smoothing;
+        [SerializeField] private float smoothing;
 
         private Transform _target;
-        private Vector3 offset;
-
-        private void Start() =>
-            offset = transform.rotation * Vector3.back * _distance;
+        private Transform _parentTransform;
 
         public void Construct(Transform target)
         {
             _target = target;
+            _parentTransform = transform.parent;
             enabled = true;
         }
 
         protected override void LateRun()
         {
-            Vector3 targetCamPos = _target.position + offset + _cameraOffset;
-            transform.position = Vector3.Lerp(transform.position,
-                targetCamPos,
-                _smoothing * Time.deltaTime);
+            _parentTransform.position = Vector3.Lerp(
+                _parentTransform.position,
+                _target.position,
+                smoothing * Time.smoothDeltaTime);
         }
     }
 }
