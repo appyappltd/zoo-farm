b771cde390de52de5c63a1a7da4a74fdfcede371
redlroses <redlroses.rlr@gmail.com>
2023-06-06 16:39:57 +0500

    refactor sineAnimate

--- Assets/Code/Logic/SineAnimate.cs
+++ Assets/Code/Logic/SineAnimate.cs
@@ -1,18 +1,22 @@
 using NTC.Global.Cache;
+using Tools.Constants;
 using UnityEngine;
 
 namespace Logic
 {
     public class SineAnimate : MonoCache
     {
+        private const float Offset = 0.5f;
+
         [SerializeField] private float _speed = 2;
         [SerializeField] private float _min = 1.5f;
-        [SerializeField] private float  _max = 2;
+        [SerializeField] private float _max = 2;
         [SerializeField] private Vector3 _movement;
-        [SerializeField] private Vector3 _scale = new(1,1,0);
+        [SerializeField] private Vector3 _scale = new(1, 1, 0);
 
         private float t;
-        private Vector3 startPos, startScale;
+        private Vector3 startPos;
+        private Vector3 startScale;
 
         private void Start()
         {
@@ -23,8 +27,8 @@ namespace Logic
 
         protected override void Run()
         {
-            t = (t + Time.fixedDeltaTime * _speed) % (Mathf.PI * 2);
-            var k = _min + (Mathf.Sin(t) / 2 + 0.5f) * (_max - _min);
+            t = (t + Time.deltaTime * _speed) % Trigonometry.TwoPiGrade;
+            var k = _min + (Mathf.Sin(t) * Arithmetic.ToHalf + Offset) * (_max - _min);
             transform.localPosition = startPos + _movement * k;
             transform.localScale = new Vector3(
                 Mathf.Lerp(startScale.x, k, _scale.x),
