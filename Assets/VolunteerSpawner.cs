17da066eca6e416080cb15a7f976f129d85774f9
Егор Агеев <egor.ageev@mail.ru>
2023-06-07 15:59:51 +0500

    Random Spawn Volunteers

--- Assets/VolunteerSpawner.cs
+++ Assets/VolunteerSpawner.cs
@@ -7,7 +7,7 @@ public class VolunteerSpawner : MonoBehaviour
 {
     public event UnityAction<VolunteerMovement> SpawnV;
 
-    [SerializeField, Min(.0f)] private float _time = 10f;
+    [SerializeField, Min(.0f)] private Vector2 _time = new(5,40);
     [SerializeField] private Transform _spawnPlace;
 
     [SerializeField] private VolunteerMovement _volunteer;
@@ -28,7 +28,7 @@ public class VolunteerSpawner : MonoBehaviour
             var v = Instantiate(_volunteer, _spawnPlace.position,
                                             _spawnPlace.rotation);
             SpawnV?.Invoke(v);
-            yield return new WaitForSeconds(_time);
+            yield return new WaitForSeconds(Random.Range(5,40));
         }
     }
 }
