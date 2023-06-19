a8a156ea8a505e5359316d8c65f60fd891d77818
Егор Агеев <egor.ageev@mail.ru>
2023-06-19 15:30:38 +0500

    Change Delay Logic

--- Assets/Code/Logic/Interactions/Delay.cs
+++ Assets/Code/Logic/Interactions/Delay.cs
@@ -2,6 +2,7 @@ using System;
 using Logic.Interactions;
 using System.Collections;
 using UnityEngine;
+using static UnityEditor.Timeline.Actions.MenuPriority;
 
 [RequireComponent(typeof(TriggerObserver))]
 public class Delay : MonoBehaviour
@@ -10,16 +11,44 @@ public class Delay : MonoBehaviour
 
     [SerializeField, Min(.0f)] private float _delay = 1f;
 
+    [SerializeField] private float time = .0f;
+    private Coroutine coroutine;
+
     private void Awake()
     {
         var trigger = GetComponent<TriggerObserver>();
-        trigger.Enter += obj => StartCoroutine(Wait(obj));
-        trigger.Exit += _ => StopAllCoroutines();
+        trigger.Enter += obj =>
+        {
+            if (coroutine != null)
+                StopCoroutine(coroutine);
+            coroutine = StartCoroutine(Wait(obj));
+        };
+        trigger.Exit += _ =>
+        {
+            if (coroutine != null)
+                StopCoroutine(coroutine);
+            coroutine = StartCoroutine(Reverse());
+        };
     }
 
     private IEnumerator Wait(GameObject obj)
     {
-        yield return new WaitForSeconds(_delay);
+        while (_delay > time)
+        {
+            var addTime = Time.deltaTime;
+            time += addTime;
+            yield return new WaitForSeconds(addTime);
+        }
         Complete.Invoke(obj);
+    }
+
+    private IEnumerator Reverse()
+    {
+        while (time > 0) 
+        {
+            var addTime = Time.deltaTime;
+            time -= addTime;
+            yield return new WaitForSeconds(addTime);
+        }
     }
 }
---
+++ Assets/Code/Logic/StaticSine.cs
@@ -0,0 +1,52 @@
+using Logic.Interactions;
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class StaticSine : MonoBehaviour
+{
+    [SerializeField] private Vector3 _targetSize;
+    [SerializeField, Min(.0f)] private float _speed =.1f;
+    [SerializeField, Min(.0f)] private float _offset = .1f;
+    [SerializeField] private TriggerObserver _trigger;
+
+    private Vector3 defSize;
+    private Coroutine coroutine;
+    private Vector3 lastSc = new(121,432,543);
+
+    private void Awake()
+    {
+        defSize = transform.localScale;
+
+        _trigger.Enter += _ => OnInteractable(_targetSize);
+        _trigger.Exit += _ => OnInteractable(defSize);
+    }
+
+    [Button("расти", enabledMode: EButtonEnableMode.Playmode)]
+    public void A() => OnInteractable(_targetSize);
+
+    [Button("Уменьшайся", enabledMode: EButtonEnableMode.Playmode)]
+    public void B() => OnInteractable(defSize);
+
+    public void OnInteractable(Vector3 size)
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+        coroutine = StartCoroutine(ChangeSize(size));
+    }
+
+    private IEnumerator ChangeSize(Vector3 targetSize)
+    {
+        while ((transform.localScale - targetSize).magnitude > _offset 
+            && lastSc != transform.localScale)
+        {
+            lastSc = transform.localScale;
+            transform.localScale = Vector3.MoveTowards(transform.localScale,
+                                                     targetSize,
+                                                     _speed * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
