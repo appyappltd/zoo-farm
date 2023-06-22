f364beeac175da1839cd7f9b59c1ef872ad87c04
redlroses <redlroses.rlr@gmail.com>
2023-06-22 13:48:58 +0500

    cleanup

--- Assets/Code/Logic/CellBuilding/BuildCell.cs
+++ Assets/Code/Logic/CellBuilding/BuildCell.cs
@@ -1,4 +1,5 @@
 using System;
+using Logic.Wallet;
 using Tools.Extension;
 using UnityEngine;
 
---
+++ Assets/Code/Logic/Wallet/Consumer.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using Infrastructure.Factory;
+using Logic.Interactions;
+using Logic.Spawners;
+using Logic.Translators;
+using Observables;
+using Player;
+using Services;
+using UnityEngine;
+
+namespace Logic.Wallet
+{
+    [RequireComponent(typeof(RunTranslator))]
+    [RequireComponent(typeof(Delay))]
+    public class Consumer : MonoBehaviour
+    {
+        public event Action Bought = () => { };
+
+        [SerializeField, Min(0f)] private float _time = .1f;
+
+        private int _defaultCost;
+        private VisualTranslatorsSpawner spawner;
+        private RunTranslator translator;
+        private TriggerObserver trigger;
+        private Wallet wallet;
+        private Observable<int> leftCoinsToPay;
+
+        public Observables.IObservable<int> LeftCoinsToPay => leftCoinsToPay;
+
+        private void Awake()
+        {
+            leftCoinsToPay = new Observable<int>();
+
+            translator = GetComponent<RunTranslator>();
+            trigger = GetComponent<TriggerObserver>();
+
+            trigger.Enter += Init;
+            trigger.Exit += _ => StopAllCoroutines();
+
+            GetComponent<Delay>().Complete += _ => StartCoroutine(TakeCoins());
+        }
+
+        public void SetCost(int buildCost)
+        {
+            _defaultCost = buildCost;
+            leftCoinsToPay.Value = buildCost;
+        }
+
+        private void Init(GameObject player)
+        {
+            spawner = new VisualTranslatorsSpawner(() =>
+                    AllServices.Container.Single<IGameFactory>().CreateVisual(VisualType.Money,
+                        Quaternion.identity,
+                        new GameObject("Coins").transform),
+                10, translator, player.transform,
+                transform);
+        
+            wallet = player.GetComponent<HeroWallet>().Wallet;
+        
+            trigger.Enter -= Init;
+        }
+
+        private IEnumerator TakeCoins()
+        {
+            while (leftCoinsToPay.Value > 0)
+            {
+                if (!wallet.TrySpend(1))
+                    break;
+
+                leftCoinsToPay.Value--;
+
+                var translatable = spawner.Spawn().MainTranslatable;
+
+                if (leftCoinsToPay.Value == 0)
+                    translatable.End += Buy;
+            
+                yield return new WaitForSeconds(_time);
+            }
+        }
+
+        private void Buy(ITranslatable translatable)
+        {
+            Bought.Invoke();
+            translatable.End -= Buy;
+            leftCoinsToPay.Value = _defaultCost;
+        }
+    }
+}
