18af0f7c2dea1a0bd0cf9df061f5e2cdf908bfd6
redlroses <redlroses.rlr@gmail.com>
2023-06-28 10:28:14 +0500

    Add turn enum

--- Assets/Code/Logic/Animals/AnimalsBehaviour/AnimalStats/ProgressBarIndicator.cs
+++ Assets/Code/Logic/Animals/AnimalsBehaviour/AnimalStats/ProgressBarIndicator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using NaughtyAttributes;
 using NTC.Global.Cache;
@@ -10,9 +11,8 @@ namespace Logic.Animals.AnimalsBehaviour.AnimalStats
     {
         [SerializeField] [Range(0, 100f)] private float _maxValue;
         [SerializeField] [Range(0, 1f)] private float _changeSpeed;
-        [SerializeField] private bool _isSpend;
+        [SerializeField] private Turn _turn;
         [SerializeField] [Range(0, 100f)] private float _startValue;
-        
 
         private ProgressBar _progressBar;
         private ProgressBarOperator _progressBarOperator;
@@ -22,7 +22,7 @@ namespace Logic.Animals.AnimalsBehaviour.AnimalStats
         private void Awake()
         {
             _progressBar = new ProgressBar(_maxValue, _startValue);
-            _progressBarOperator = new ProgressBarOperator(_progressBar, _changeSpeed, _isSpend);
+            _progressBarOperator = new ProgressBarOperator(_progressBar, _changeSpeed, Convert.ToBoolean(_turn));
         }
 
         protected override void OnEnabled() =>
---
+++ Assets/Code/Logic/Animals/AnimalsBehaviour/AnimalStats/Turn.cs
@@ -0,0 +1,8 @@
+namespace Logic.Animals.AnimalsBehaviour.AnimalStats
+{
+    public enum Turn
+    {
+        Forward = 0,
+        Reverse = 1,
+    }
+}
