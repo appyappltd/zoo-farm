eb30a328a74633bc662b8df54a6af96f51394d78
redlroses <redlroses.rlr@gmail.com>
2023-06-30 13:32:40 +0500

    monir fix

---
+++ Assets/Code/Logic/Volunteer/TestVolunteerCreator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Logic.Volunteer
+{
+    [RequireComponent(typeof(TimerOperator))]
+    public class TestVolunteerCreator : MonoBehaviour
+    {
+        [SerializeField] private float _spawnDelay = 10f;
+        [SerializeField] private int _maxCount = 3;
+        [SerializeField] private VolunteerBand _volunteerBand;
+
+        private int _activeCount;
+        private TimerOperator _timer;
+
+        private void Awake()
+        {
+            _timer = GetComponent<TimerOperator>();
+        }
+
+        private void Start()
+        {
+            _timer.SetUp(_spawnDelay, OnSpawn);
+            _timer.Restart();
+        }
+
+        private void OnSpawn()
+        {
+            if (_activeCount <= _maxCount)
+            {
+                _volunteerBand.CreateNewVolunteer();
+                _activeCount++;
+            }
+
+            _timer.Restart();
+        }
+    }
+}
