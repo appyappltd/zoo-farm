e4520afcb4cdee6d2045107b9c26627c88344c71
redlroses <redlroses.rlr@gmail.com>
2023-07-28 16:36:38 +0500

    Fix tool animation

--- Assets/Code/Logic/Translators/Translator.cs
+++ Assets/Code/Logic/Translators/Translator.cs
@@ -21,6 +21,14 @@ namespace Logic.Translators
             enabled = true;
         }
 
+        public void RemoveTranslatable(ITranslatable translatable)
+        {
+            _translatables.Remove(translatable);
+
+            if (_translatables.Count <= 0)
+                enabled = false;
+        }
+
         protected void UpdateTranslatable()
         {
             for (var index = 0; index < _translatables.Count; index++)
@@ -32,14 +40,6 @@ namespace Logic.Translators
                     RemoveTranslatable(translatable);
                 }
             }
-        }
-
-        private void RemoveTranslatable(ITranslatable translatable)
-        {
-            _translatables.Remove(translatable);
-
-            if (_translatables.Count <= 0)
-                enabled = false;
         }
     }
 }
