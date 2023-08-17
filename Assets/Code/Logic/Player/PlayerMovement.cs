942b12a858d1081628dd427f2438f49fad16ed2c
redlroses <redlroses.rlr@gmail.com>
2023-08-17 15:56:27 +0500

    Change moving dust effect

--- Assets/Code/Logic/Player/PlayerMovement.cs
+++ Assets/Code/Logic/Player/PlayerMovement.cs
@@ -6,7 +6,6 @@ namespace Logic.Player
 {
     public class PlayerMovement : MonoCache
     {
-        [SerializeField] private ParticleSystem particleS;
         [SerializeField] private HeroAnimator _animator;
         [SerializeField] private float _speed;
 
@@ -20,10 +19,8 @@ namespace Logic.Player
             _animator.SetSpeed(_speed);
         }
 
-        private void OnValidate()
-        {
+        private void OnValidate() =>
             _animator.SetSpeed(_speed);
-        }
 
         public void Construct(IPlayerInputService inputService) =>
             _inputService = inputService;
@@ -33,15 +30,13 @@ namespace Logic.Player
             if (_inputService.Direction.Equals(Vector2.zero))
             {
                 _animator.SetMove(false);
-                particleS.enableEmission = false;
                 return;
             }
         
             _moveDirection = new Vector3(_inputService.Horizontal, _moveDirection.y, _inputService.Vertical);
             _moveDirection = CameraRotation() * _moveDirection;
             _animator.SetMove(true);
-            particleS.enableEmission = true;
-            var angle = Mathf.Rad2Deg * Mathf.Atan2(_moveDirection.x, _moveDirection.z);
+            float angle = Mathf.Rad2Deg * Mathf.Atan2(_moveDirection.x, _moveDirection.z);
             transform.rotation = Quaternion.Euler(0, angle, 0);
         }
 
