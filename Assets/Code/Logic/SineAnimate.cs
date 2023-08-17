0237f94ee21f902124e0e9ccabb78466a793e198
redlroses <redlroses.rlr@gmail.com>
2023-08-17 11:34:25 +0500

    Change Sprites sorting layer and fix house icon sizing

--- Assets/Code/Logic/SineAnimate.cs
+++ Assets/Code/Logic/SineAnimate.cs
@@ -6,34 +6,24 @@ namespace Logic
 {
     public class SineAnimate : MonoCache
     {
-        private const float Offset = 0.5f;
-
+        [SerializeField] private SpriteRenderer _sprite;
         [SerializeField] private float _speed = 2;
         [SerializeField] private float _min = 1.5f;
         [SerializeField] private float _max = 2;
-        [SerializeField] private Vector3 _movement;
-        [SerializeField] private Vector3 _scale = new(1, 1, 0);
-
-        private float t;
-        private Vector3 startPos;
-        private Vector3 startScale;
+        
+        private Vector2 _startScale;
 
         private void Start()
         {
-            var selfTransform = transform;
-            startPos = selfTransform.localPosition;
-            startScale = selfTransform.localScale;
+            _sprite.drawMode = SpriteDrawMode.Sliced;
+            _startScale = _sprite.size;
         }
 
         protected override void Run()
         {
-            t = (t + Time.deltaTime * _speed) % Trigonometry.TwoPiGrade;
-            var k = _min + (Mathf.Sin(t) * Arithmetic.ToHalf + Offset) * (_max - _min);
-            transform.localPosition = startPos + _movement * k;
-            transform.localScale = new Vector3(
-                Mathf.Lerp(startScale.x, k, _scale.x),
-                Mathf.Lerp(startScale.y, k, _scale.y),
-                Mathf.Lerp(startScale.z, k, _scale.z));
+            float sine = Mathf.Sin(Time.time * _speed);
+            float scaleModifier = (sine + 1) * Arithmetic.ToHalf;
+            _sprite.size = _startScale * Mathf.Lerp(_min, _max, scaleModifier);
         }
     }
 }
