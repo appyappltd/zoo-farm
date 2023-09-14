0c43351185c89ae5f3868f00b1955067d4624118
redlroses <redlroses.rlr@gmail.com>
2023-09-14 16:27:28 +0500

    Add toward item moving

--- Assets/Code/Logic/Movement/ItemMover.cs
+++ Assets/Code/Logic/Movement/ItemMover.cs
@@ -2,12 +2,15 @@ using System;
 using NaughtyAttributes;
 using NTC.Global.Cache;
 using NTC.Global.System;
+using Tools;
 using UnityEngine;
 
 namespace Logic.Movement
 {
     public class ItemMover : MonoCache, IItemMover
     {
+        private const float MinDistance = 0f;
+        
         [SerializeField] [Min(.0f)] private float _speed = 5.0f;
         [SerializeField] [Min(.0f)] private float _errorOffset = 0.1f;
         [SerializeField] [MinValue(0f)] [MaxValue(1f)] private float _power = 0.5f;
@@ -24,6 +27,7 @@ namespace Logic.Movement
 
         private bool _isModifyRotation;
         private DistanceBasedScaleModifier _scaleModifier;
+        private TowardMover<float> _towardMover;
 
         public event Action Started = () => { };
         public event Action Ended = () => { };
@@ -32,8 +36,9 @@ namespace Logic.Movement
 
         private void Awake()
         {
+            _towardMover = new TowardMover<float>(0f, 1f, Mathf.Lerp, AnimationCurve.Linear(0f, 0f,1f,1f));
             _scaleModifier = new DistanceBasedScaleModifier();
-            _moving = LerpTranslate;
+            _moving = TowardTranslate;
             enabled = false;
         }
 
@@ -59,6 +64,8 @@ namespace Logic.Movement
                 _moving += UpdateScale;
             }
             
+            _towardMover.Reset();
+            _towardMover.Forward();
             _isModifyRotation = isModifyRotation;
             transform.Unparent();
             enabled = true;
@@ -75,6 +82,16 @@ namespace Logic.Movement
                 FinishTranslation();
         }
 
+        private void TowardTranslate(float distance)
+        {
+            _towardMover.TryUpdate(Time.deltaTime * _speed, out float lerped);
+            
+            transform.position = Vector3.Lerp(
+                transform.position,
+                Target.position,
+                lerped);
+        }
+        
         private void Rotate(float distanceToTarget)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, GetFinalRotation(),
@@ -86,10 +103,10 @@ namespace Logic.Movement
             transform.position = Vector3.Lerp(
                 transform.position,
                 Target.position,
-                _speed * Time.smoothDeltaTime / Mathf.Pow(distance, 1f - _power));
+                _speed * Time.deltaTime / Mathf.Pow( Mathf.Max(distance, MinDistance), 1f - _power));
         }
         
-        private void Translate(float _)
+        private void Translate(float distance)
         {
             Vector3 translateDirection = (Target.position - transform.position).normalized;
             Vector3 deltaTranslation = translateDirection * _speed * Time.smoothDeltaTime;
